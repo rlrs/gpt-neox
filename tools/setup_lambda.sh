3d40dac51775c88fef75c6a3a1c23803750ff019
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2023-05-21 17:36:15 +0200

    flash attn and apex

--- tools/setup_lambda.sh
+++ tools/setup_lambda.sh
@@ -7,4 +7,8 @@ python -m venv .venv/
 pip3 install torch --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements/requirements.txt
 pip install -r requirements/requirements-wandb.txt
+pip install -r requirements/requirements-flashattention.txt
 python ./megatron/fused_kernels/setup.py install
+
+## Install APEX
+pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" git+https://github.com/NVIDIA/apex.git@a651e2c24ecf97cbf367fd3f330df36760e1c597
